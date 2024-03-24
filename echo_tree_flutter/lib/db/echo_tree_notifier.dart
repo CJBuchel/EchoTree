2a96cc0c429f2d72f7745db4d24246d6749b6e88
CJBuchel <connorbuchel@gmail.com>
2024-03-25 00:39:47 +0800

    Added EchoTree widgets

--- echo_tree_flutter/lib/db/echo_tree_notifier.dart
+++ echo_tree_flutter/lib/db/echo_tree_notifier.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 
 import 'package:echo_tree_flutter/db/managed_tree.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
 class EchoTreeNotifier<K, V> extends ChangeNotifier {
   final ManagedTree managedTree;
@@ -37,5 +39,41 @@ class EchoTreeNotifier<K, V> extends ChangeNotifier {
   void dispose() {
     _updatesStream.cancel();
     super.dispose();
+  }
+}
+
+typedef EchoTreeDetailWidgetBuilder<V> = Widget Function(BuildContext context, V? value);
+typedef EchoTreeMapWidgetBuilder<K, V> = Widget Function(BuildContext context, Map<K, V> items);
+
+class EchoTreeDetailWidget<K, V> extends StatelessWidget {
+  final K detailKey;
+  final EchoTreeDetailWidgetBuilder<V> builder;
+  const EchoTreeDetailWidget({super.key, required this.detailKey, required this.builder});
+
+  @override
+  Widget build(BuildContext context) {
+    return Selector<EchoTreeNotifier<K, V>, V?>(
+      selector: (_, nt) => nt.items[detailKey],
+      shouldRebuild: (previous, next) => previous != next,
+      builder: (context, value, _) {
+        return builder(context, value);
+      },
+    );
+  }
+}
+
+class EchoTreeMapWidget<K, V> extends StatelessWidget {
+  final EchoTreeMapWidgetBuilder<K, V> builder;
+  const EchoTreeMapWidget({super.key, required this.builder});
+
+  @override
+  Widget build(BuildContext context) {
+    return Selector<EchoTreeNotifier<K, V>, Map<K, V>>(
+      selector: (_, nt) => nt.items,
+      shouldRebuild: (previous, next) => previous != next,
+      builder: (context, items, _) {
+        return builder(context, items);
+      },
+    );
   }
 }
